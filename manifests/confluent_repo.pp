c621be23cb2ad5aa80370d464264f13d09f4fb42
Ryan Dill <rdill@unified.com>
2023-12-13 04:28:53 +0000

    1.0.0: added apt repo

--- manifests/confluent_repo.pp
+++ manifests/confluent_repo.pp
@@ -5,15 +5,35 @@
 class kafka_connect::confluent_repo {
   assert_private()
 
-  yumrepo { 'confluent':
-    ensure              => $kafka_connect::repo_ensure,
-    baseurl             => "http://packages.confluent.io/rpm/${kafka_connect::repo_version}",
-    name                => 'Confluent',
-    descr               => 'Confluent repository',
-    enabled             => $kafka_connect::repo_enabled,
-    gpgcheck            => '1',
-    gpgkey              => "http://packages.confluent.io/rpm/${kafka_connect::repo_version}/archive.key",
-    skip_if_unavailable => '1',
+  if $facts['os']['family'] == 'RedHat' {
+    yumrepo { 'confluent':
+      ensure              => $kafka_connect::repo_ensure,
+      baseurl             => "http://packages.confluent.io/rpm/${kafka_connect::repo_version}",
+      name                => 'Confluent',
+      descr               => 'Confluent repository',
+      enabled             => $kafka_connect::repo_enabled,
+      gpgcheck            => '1',
+      gpgkey              => "http://packages.confluent.io/rpm/${kafka_connect::repo_version}/archive.key",
+      skip_if_unavailable => '1',
+    }
+
+  } elsif $facts['os']['family'] == 'Debian' {
+    apt::source { 'confluent':
+      ensure   => $kafka_connect::repo_ensure,
+      comment  => 'Confluent repository',
+      location => "https://packages.confluent.io/deb/${kafka_connect::repo_version}",
+      release  => 'stable',
+      repos    => 'main',
+      key      => {
+        id     => 'CBBB821E8FAF364F79835C438B1DA6120C2BF624',
+        source => "https://packages.confluent.io/deb/${kafka_connect::repo_version}/archive.key"
+      },
+    }
+
+    Apt::Source['confluent'] -> Class['apt::update'] -> Class[$kafk_connect::install]
+
+  } else {
+    fail(sprintf('Confluent Repository is not supported on %s', $facts['os']['family']))
   }
 
 }
