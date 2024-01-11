1a36281fe09e557f5cf969d573566ef826b0e9c8
Ryan Dill <rdill@unified.com>
2024-01-11 06:09:14 +0000

    1.1.0: updates to support setting ensure in secrets data

--- manifests/manage_connectors.pp
+++ manifests/manage_connectors.pp
@@ -62,14 +62,30 @@ class kafka_connect::manage_connectors {
     $secrets_data.each |$secret| {
 
       $secret_file_name  = $secret[0]
+      $secret_ensure     = $secret[1]['ensure']
       $secret_connectors = $secret[1]['connectors']
       $secret_key        = $secret[1]['key']
       $secret_value      = $secret[1]['value']
 
-      $secret_content = Sensitive("${secret_key}=${secret_value}\n")
+      if $secret_ensure {
+        unless $secret_ensure =~ /^(absent|present|file)$/ {
+          fail("Unexpected ensure value encountered for ${secret_file_name}: ${secret_ensure} \
+            \n This should be one of 'absent', 'present', or 'file' \n")
+        }
+
+        $secret_file_ensure = $secret_ensure
+      } else {
+        $secret_file_ensure = 'present'
+      }
+
+      if $secret_file_ensure == 'absent' {
+        $secret_content = undef
+      } else {
+        $secret_content = Sensitive("${secret_key}=${secret_value}\n")
+      }
 
       file { $secret_file_name :
-        ensure  => 'present',
+        ensure  => $secret_file_ensure,
         path    => "${kafka_connect::connector_config_dir}/${secret_file_name}",
         content => $secret_content,
         owner   => $kafka_connect::owner,
