dbd653d85fdbaa2e31c2551dc7a2e4353706ac4b
Ryan Dill <rdill@unified.com>
2024-01-25 02:30:58 +0000

    some final data type updates

--- types/package_ensure.pp
+++
@@ -1,2 +0,0 @@
-# @summary This type accepts (nearly) all possible package resource ensure options
-type Kafka_connect::Package_ensure = Variant[SemVer, Enum['present', 'installed', 'latest', 'absent', 'purged']]
