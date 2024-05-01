c49019c4b1f20651df1980654d06fbc62208967b
Ryan Dill <rdill@unified.com>
2024-05-01 12:50:10 +0800

    added class exclusion test

--- spec/classes/manage_connectors_spec.rb
+++ spec/classes/manage_connectors_spec.rb
@@ -6,14 +6,20 @@ describe 'kafka_connect::manage_connectors' do
   on_supported_os.each do |os, facts|
     context "on #{os}" do
       let(:facts) { facts }
-      let(:hiera_config) { 'hiera-rspec.yaml' }
       let(:pre_condition) { 'include kafka_connect' }
 
       it { is_expected.to compile.with_all_deps }
 
       describe 'default, with connector and secret data' do
+        let(:hiera_config) { 'hiera-rspec.yaml' }
+
         it { is_expected.to contain_class('kafka_connect::manage_connectors::connector') }
         it { is_expected.to contain_class('kafka_connect::manage_connectors::secret') }
+      end
+
+      describe 'default, with no connector or secret data' do
+        it { is_expected.not_to contain_class('kafka_connect::manage_connectors::connector') }
+        it { is_expected.not_to contain_class('kafka_connect::manage_connectors::secret') }
       end
     end
   end
